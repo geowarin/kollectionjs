9ecb7fd5107b1c801dd6a35e713f7ab9d310877b
geowarin <geowarin@gmail.com>
2017-12-06 14:26:41 +0100

    cleanup

--- src/Sequence.ts
+++ src/Sequence.ts
@@ -1,13 +1,13 @@
 import {getIterator, isIterable} from "./utils";
 
 export class Sequence<T> implements IterableIterator<T> {
-  _next: () => IteratorResult<T>;
+  private readonly _next: () => IteratorResult<T>;
 
   public next(): IteratorResult<T> {
     return this._next()
   }
 
-  constructor(private iterator: Iterator<T>) {
+  constructor(iterator: Iterator<T>) {
     this._next = iterator.next.bind(iterator);
   }
 
@@ -17,59 +17,55 @@ export class Sequence<T> implements IterableIterator<T> {
 
   filter(predicate: (item: T) => boolean): Sequence<T> {
     return this.rewrap(function* (this: Iterable<T>) {
-      for (let val of this) {
-        if (predicate(val))
-          yield val;
+      for (let item of this) {
+        if (predicate(item)) {
+          yield item;
+        }
       }
     });
   }
 
   map<U, S>(transform: (element: T) => S): Sequence<S> {
-    return this.rewrap(
-      function* (this: Iterable<T>) {
-        for (let val of this) {
-          yield transform(val);
-        }
+    return this.rewrap(function* (this: Iterable<T>) {
+      for (let item of this) {
+        yield transform(item);
       }
-    );
+    });
   }
 
   flatten() {
     return this.rewrap(function* (this: Iterable<T>) {
-      for (let x of this) {
-        if (typeof x !== "string" && isIterable(x)) {
-          yield* x;
+      for (let item of this) {
+        if (typeof item !== "string" && isIterable(item)) {
+          yield* item;
         } else {
-          yield x;
+          yield item;
         }
       }
-    })
+    });
   }
 
   take(num: number = 1): Sequence<T> {
-    return this.rewrap((
-      function* (this: Iterable<T>) {
-        let count = 0;
-        for (let val of this) {
-          if (count++ >= num) {
-            break;
-          }
-          yield val;
+    return this.rewrap(function* (this: Iterable<T>) {
+      let count = 0;
+      for (let item of this) {
+        if (count++ >= num) {
+          break;
         }
+        yield item;
       }
-    ));
+    });
   }
 
   drop(num: number = 1): Sequence<T> {
     return this.rewrap(function* (this: Iterable<T>) {
-        let count = 0;
-        for (let val of this) {
-          if (count++ >= num) {
-            yield val;
-          }
+      let count = 0;
+      for (let item of this) {
+        if (count++ >= num) {
+          yield item;
         }
       }
-    );
+    });
   }
 
   groupBy<K>(keySelector: (value: T) => K): Map<K, T[]> {
