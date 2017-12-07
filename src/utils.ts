3a6a7ca50d40886bfa759eee9d9a6c488dbf8bf3
geowarin <geowarin@gmail.com>
2017-12-07 14:12:10 +0100

    Remove unused functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,3 @@
-export function getIterableIterator<T>(iterable: IterableIterator<T> | Array<T>): IterableIterator<T> {
-  return iterable [Symbol.iterator]()
-}
 
 export function getIterator<T>(iterable: Iterable<T>): Iterator<T> {
   return iterable [Symbol.iterator]()
@@ -8,10 +5,6 @@ export function getIterator<T>(iterable: Iterable<T>): Iterator<T> {
 
 export function isIterable(obj: any): obj is Iterable<any> {
   return obj != null && typeof obj[Symbol.iterator] === 'function';
-}
-
-export function entries<T>(obj: { [k: string]: T }) {
-  return Object.keys(obj).map(key => [key, obj[key]]);
 }
 
 export function contains<T>(iterable: Iterable<T>, element: T): boolean {
