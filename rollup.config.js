34b097316443b9117f61ff164fd61332958b2fae
geowarin <geowarin@gmail.com>
2017-12-07 08:35:06 +0100

    Add commonjs and es builds

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,30 @@ import typescript from 'rollup-plugin-typescript2';
 export default [
     {
         input: 'src/index.ts',
-        output: {
-            file: "dist/kollection-js.umd.es6.js",
-            format: 'umd'
-        },
+        output: {file: pkg.browser, format: 'umd'},
         name: 'Kollection',
         plugins: [
+            typescript()
+        ]
+    },
+    {
+        input: 'src/index.ts',
+        output: [
+            { file: pkg.main, format: 'cjs' },
+        ],
+        plugins: [
             typescript({
-                target: "esnext"
+                target: "es6"
             })
+        ]
+    },
+    {
+        input: 'src/index.ts',
+        output: [
+            {file: pkg.module, format: 'es'}
+        ],
+        plugins: [
+            typescript()
         ]
     }
 ];
